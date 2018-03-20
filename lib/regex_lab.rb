2f2461361be94a0f746af2cb92d2f1960f744545
michael hohl <mhohl@rocketmail.com>
2018-03-20 20:49:32 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,5 @@
 def starts_with_a_vowel?(word)
-  word.downcase.match(/\b[aeiouAEIOU]/g) != nil
+  word.downcase.match(/^[aeiouAEIOU].*/g) != nil
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
